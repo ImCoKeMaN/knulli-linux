e8c1b5f7a0b0484fb9c5f3c1a68083171ec440f6
Mikhailzrick <90733003+Mikhailzrick@users.noreply.github.com>
2025-10-24 07:38:54 -0500

    yabasanshiro-update
    New configgen for generating user inputs. Should work with external controllers. Fixes ARC internal tag in es input. Fix evmapy keys. Add HLE bios es setting.

--- package/system/knulli-configgen/configgen/configgen/generators/yabasanshiro/yabasanshiroGenerator.py
+++ package/system/knulli-configgen/configgen/configgen/generators/yabasanshiro/yabasanshiroGenerator.py
@@ -1,41 +1,118 @@
 from __future__ import annotations
 
-import xml.etree.ElementTree as ET
-
-from typing import TYPE_CHECKING
+import logging
+import json
+from pathlib import Path
+from typing import TYPE_CHECKING, Final, Dict, Any
 
 from ... import Command
+from ...batoceraPaths import BIOS, HOME, SAVES, ensure_parents_and_open
 from ...controller import generate_sdl_game_controller_config
 from ..Generator import Generator
-from ...batoceraPaths import ES_SETTINGS
 
 if TYPE_CHECKING:
     from ...types import HotkeysContext
+    from ...controller import ControllerMapping
+
+eslog = logging.getLogger(__name__)
+
+YABA_KEYMAP: Final = HOME / ".yabasanshiro" / "keymapv2.json"
+YABA_BIOS: Final = BIOS / "saturn_bios.bin"
+
+UNBOUND = {"id": -1, "type": "", "value": -999}
+HAT_VAL = {"up": 1, "right": 2, "down": 4, "left": 8}
+
+ES_TO_YABA = {
+    "a": "b",
+    "b": "a",
+    "x": "y",
+    "y": "x",
+    "pageup": "z",
+    "pagedown": "c",
+    "l2": "l",
+    "r2": "r",
+    "start": "start",
+    "select": "select",
+    "up": "up", "down": "down", "left": "left", "right": "right",
+    "joystick1left": "analogx",
+    "joystick1up": "analogy",
+}
+
+def generateYabaKeymap(playersControllers: ControllerMapping):
+    store = {}
+
+    for player_index in (1, 2):
+        if player_index not in playersControllers:
+            continue
+
+        controller = playersControllers[player_index]
+        name = controller.name
+        guid = controller.guid
+        device_id = controller.index
+        dev_key = f"{device_id}_{name}_{guid}"
+
+        actions = ["a","b","c","x","y","z","l","r","start","select",
+                "up","down","left","right","analogx","analogy","analogleft","analogright"]
+        block = {k: dict(UNBOUND) for k in actions}
+
+        for idx in controller.inputs:
+            inp = controller.inputs[idx]
+            src = inp.name
+            if src not in ES_TO_YABA:
+                continue
+            action = ES_TO_YABA[src]
+
+            if inp.type == "button":
+                block[action] = {"id": int(inp.id), "type": "button", "value": 1}
+
+            elif inp.type == "hat" and action in ("up","down","left","right"):
+                block[action] = {"id": int(inp.id), "type": "hat", "value": HAT_VAL[action]}
+
+            elif inp.type == "axis":
+                v = -1 if int(inp.value) < 0 else (1 if int(inp.value) > 0 else 0)
+
+                if src == "joystick1left":
+                    block["analogx"] = {"id": int(inp.id), "type": "axis", "value": v}
+
+                elif src == "joystick1up":
+                    block["analogy"] = {"id": int(inp.id), "type": "axis", "value": v}
+                else:
+                    block[action] = {"id": int(inp.id), "type": "axis", "value": v}
+
+        if block["analogx"]["id"] == -1:
+            block["analogx"] = {"id": 0, "type": "axis", "value": 0}
+        if block["analogy"]["id"] == -1:
+            block["analogy"] = {"id": 1, "type": "axis", "value": 0}
+
+        store[dev_key] = block
+        store[f"player{player_index}"] = {
+            "DeviceID": device_id,
+            "deviceGUID": guid,
+            "deviceName": name,
+            "padmode": 0
+        }
+
+    with ensure_parents_and_open(YABA_KEYMAP, "w") as f:
+        json.dump(store, f, indent=2, ensure_ascii=False)
+    return YABA_KEYMAP
 
 class YabasanshiroGenerator(Generator):
 
-    # Return value for es invertedbuttons
-    def getInvertButtonsValue(self) -> bool:
-        try:
-            tree = ET.parse(ES_SETTINGS)
-            root = tree.getroot()
-            # Find the InvertButtons element and return value
-            elem = root.find(".//bool[@name='InvertButtons']")
-            if elem is not None:
-                return elem.get('value') == 'true'
-            return False  # Return False if not found
-        except:
-            return False # when file is not yet here or malformed
-
-    def generate(self, system, rom, playersControllers, metadata, guns, wheels, gameResolution):
-        commandArray = ["yabasanshiro", "-r2", "-i", rom]
-
-        return Command.Command(array=commandArray,env={
-            "SDL_GAMECONTROLLERCONFIG": generate_sdl_game_controller_config(playersControllers, self.getInvertButtonsValue())
-        })
-
     def getHotkeysContext(self) -> HotkeysContext:
         return {
-            "name": "yanbasanshiro",
+            "name": "yabasanshiro",
             "keys": { "exit": ["KEY_LEFTALT", "KEY_F4"] }
         }
+
+    def generate(self, system, rom, playersControllers, metadata, guns, wheels, gameResolution):
+        generateYabaKeymap(playersControllers)
+
+        commandArray = ["yabasanshiro", "-r", "3", "-a", "-i", rom]
+
+        if not (system.isOptSet('yaba_bios_hle') and system.config['yaba_bios_hle'] == '1'):
+            if YABA_BIOS.exists():
+                commandArray[1:1] = ["-b", str(YABA_BIOS)]
+
+        return Command.Command(array=commandArray,env={
+            "SDL_GAMECONTROLLERCONFIG": generate_sdl_game_controller_config(playersControllers)
+        })
