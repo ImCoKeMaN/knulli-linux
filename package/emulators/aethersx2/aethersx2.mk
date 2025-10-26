15d6570668c88b2d9e5b8df277f98d784d2fb381
acmeplus <ctalon@gmail.com>
2025-10-26 03:55:46 +0000

    Fixed RGB30 screen and suspendissues, and X55 suspend. Added 3DS azahar support. Update Miyoo flip SDXC support. Added AetherSX2

---
+++ package/emulators/aethersx2/aethersx2.mk
@@ -0,0 +1,56 @@
+################################################################################
+#
+# aethersx2
+#
+################################################################################
+
+AETHERSX2_VERSION = 1.0.0
+AETHERSX2_SITE = https://github.com/ROCKNIX/packages/raw/refs/heads/main
+AETHERSX2_SOURCE = aethersx2.tar.gz
+AETHERSX2_LICENSE = LGPL
+AETHERSX2_LICENSE_FILES = LICENSE
+
+AETHERSX2_DEPENDENCIES = toolchain qt6base libgpg-error libfuse xz libpcap
+
+AETHERSX2_TOOLCHAIN = manual
+
+# Let Buildroot handle extraction automatically (no custom EXTRACT_CMDS needed)
+
+define AETHERSX2_CONFIGURE_CMDS
+    # No configuration needed for prebuilt binary
+    true
+endef
+
+define AETHERSX2_BUILD_CMDS
+    # No build needed for prebuilt binary
+    true
+endef
+
+define AETHERSX2_INSTALL_TARGET_CMDS
+    mkdir -p $(TARGET_DIR)/usr/bin
+    mkdir -p $(TARGET_DIR)/usr/share/aethersx2
+    mkdir -p $(TARGET_DIR)/usr/config/aethersx2
+
+    # Install shared resources
+    if [ -d $(@D)/usr/share ]; then \
+        cp -rf $(@D)/usr/share/* $(TARGET_DIR)/usr/share/aethersx2/; \
+    fi
+
+    # Install custom sources/config
+    if [ -d $(AETHERSX2_PKG_DIR)/sources ]; then \
+        cp -rf $(AETHERSX2_PKG_DIR)/sources/* $(TARGET_DIR)/usr/share/aethersx2/; \
+    fi
+
+    # Install scripts
+    if [ -d $(AETHERSX2_PKG_DIR)/scripts ]; then \
+        cp -rf $(AETHERSX2_PKG_DIR)/scripts/* $(TARGET_DIR)/usr/bin/; \
+        chmod 755 $(TARGET_DIR)/usr/bin/*; \
+    fi
+
+    # Install device-specific config
+    if [ -d $(AETHERSX2_PKG_DIR)/config/$(KNULLI_DEVICE) ]; then \
+        cp -rf $(AETHERSX2_PKG_DIR)/config/$(KNULLI_DEVICE)/aethersx2 $(TARGET_DIR)/usr/config/; \
+    fi
+endef
+
+$(eval $(generic-package))
