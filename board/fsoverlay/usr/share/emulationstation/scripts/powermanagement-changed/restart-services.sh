e1fb935125cd63e471c4db4e3ed5c1a68c89782a
Mikhailzrick <90733003+Mikhailzrick@users.noreply.github.com>
2026-01-21 03:59:54 -0600

    esfansetting-integrate
    integrate the new es fan mode into the fan controller

--- board/fsoverlay/usr/share/emulationstation/scripts/powermanagement-changed/restart-services.sh
+++ board/fsoverlay/usr/share/emulationstation/scripts/powermanagement-changed/restart-services.sh
@@ -2,3 +2,10 @@
 
 # Restart idlewatcher daemon
 /etc/init.d/S96idlewatcher-daemon restart
+
+if [ -f /etc/init.d/S06fan-control-daemon ]; then
+    # Restart fan daemon
+    /etc/init.d/S06fan-control-daemon restart
+fi
+
+exit 0
