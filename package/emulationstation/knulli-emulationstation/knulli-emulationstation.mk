d1fe68c0fe3c3c2a6fc5a8e48708155884ce28dd
acmeplus <ctalon@gmail.com>
2026-03-17 20:42:35 +0000

    Changed ES package to use commit id instead of branch version

--- package/emulationstation/knulli-emulationstation/knulli-emulationstation.mk
+++ package/emulationstation/knulli-emulationstation/knulli-emulationstation.mk
@@ -3,8 +3,8 @@
 # knulli-emulationstation
 #
 ################################################################################
-# Last update: Commits on Oct 18, 2024
-KNULLI_EMULATIONSTATION_VERSION = knulli
+# Last update: Commits on Mar 7, 2026
+KNULLI_EMULATIONSTATION_VERSION = a6c8a2d03df990a87aca0193dab181331581ee2c
 KNULLI_EMULATIONSTATION_SITE = https://github.com/knulli-cfw/batocera-emulationstation
 KNULLI_EMULATIONSTATION_SITE_METHOD = git
 KNULLI_EMULATIONSTATION_LICENSE = MIT
