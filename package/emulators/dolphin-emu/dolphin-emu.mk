f451444aef3d45e4d9a325a70004c03e9246a463
acmeplus <ctalon@gmail.com>
2026-04-08 21:55:32 +0000

    Updated dolphin emu to use wayland+vulkan

---
+++ package/emulators/dolphin-emu/dolphin-emu.mk
@@ -0,0 +1,79 @@
+################################################################################
+#
+# dolphin-emu
+#
+################################################################################
+# Version: Commits on Apr 25, 2025
+# Add major & minor version accordingly for any bump
+DOLPHIN_EMU_VERSION = 805307f432dba079caf7339122a07de5b90c4b47
+DOLPHIN_EMU_VERSION_MAJOR = 2503a
+DOLPHIN_EMU_VERSION_MINOR = 328
+DOLPHIN_EMU_SITE = https://github.com/dolphin-emu/dolphin
+DOLPHIN_EMU_SITE_METHOD = git
+DOLPHIN_EMU_LICENSE = GPLv2+
+DOLPHIN_EMU_GIT_SUBMODULES = YES
+DOLPHIN_EMU_SUPPORTS_IN_SOURCE_BUILD = NO
+
+DOLPHIN_EMU_DEPENDENCIES = libevdev ffmpeg zlib libpng lzo libusb libcurl
+DOLPHIN_EMU_DEPENDENCIES += bluez5_utils hidapi xz host-xz sdl2 wayland wayland-protocols
+# add dolphin-triforce as a dependency so it builds first
+ifeq ($(BR2_PACKAGE_DOLPHIN_TRIFORCE),y)
+DOLPHIN_EMU_DEPENDENCIES += dolphin-triforce
+endif
+
+DOLPHIN_EMU_CONF_OPTS  = -DCMAKE_BUILD_TYPE=Release
+DOLPHIN_EMU_CONF_OPTS += -DBUILD_SHARED_LIBS=OFF
+DOLPHIN_EMU_CONF_OPTS += -DDISTRIBUTOR='batocera.linux'
+DOLPHIN_EMU_CONF_OPTS += -DUSE_DISCORD_PRESENCE=OFF
+DOLPHIN_EMU_CONF_OPTS += -DUSE_MGBA=OFF
+DOLPHIN_EMU_CONF_OPTS += -DUSE_UPNP=OFF
+DOLPHIN_EMU_CONF_OPTS += -DENABLE_TESTS=OFF
+DOLPHIN_EMU_CONF_OPTS += -DENABLE_AUTOUPDATE=OFF
+DOLPHIN_EMU_CONF_OPTS += -DENABLE_ANALYTICS=OFF
+DOLPHIN_EMU_CONF_OPTS += -DUSE_SYSTEM_LIBS=AUTO
+DOLPHIN_EMU_CONF_OPTS += -DUSE_SYSTEM_FMT=OFF
+DOLPHIN_EMU_CONF_OPTS += -DENABLE_CLI_TOOL=OFF
+DOLPHIN_EMU_CONF_OPTS += -DUSE_RETRO_ACHIEVEMENTS=ON
+
+#ifeq ($(BR2_PACKAGE_QT6),y)
+#DOLPHIN_EMU_DEPENDENCIES += qt6base qt6svg
+#DOLPHIN_EMU_CONF_OPTS += -DENABLE_QT=ON
+#DOLPHIN_EMU_CONF_OPTS += -DENABLE_NOGUI=OFF
+#else
+DOLPHIN_EMU_CONF_OPTS += -DENABLE_QT=OFF
+DOLPHIN_EMU_CONF_OPTS += -DENABLE_NOGUI=ON
+#endif
+
+DOLPHIN_EMU_MAKE_ENV += LDFLAGS="-Wl,--copy-dt-needed-entries"
+DOLPHIN_EMU_CONF_ENV += LDFLAGS="-Wl,--copy-dt-needed-entries"
+
+#ifeq ($(BR2_PACKAGE_XORG7),y)
+#    DOLPHIN_EMU_CONF_OPTS += -DENABLE_X11=ON
+#else
+    DOLPHIN_EMU_CONF_OPTS += -DENABLE_X11=OFF
+#endif
+
+DOLPHIN_EMU_CONF_OPTS += -DENABLE_WAYLAND=ON
+
+#ifeq ($(BR2_PACKAGE_BATOCERA_VULKAN),y)
+    DOLPHIN_EMU_CONF_OPTS += -DENABLE_VULKAN=ON
+#else
+#    DOLPHIN_EMU_CONF_OPTS += -DENABLE_VULKAN=OFF
+#endif
+
+define DOLPHIN_EMU_EVMAPY
+    mkdir -p $(TARGET_DIR)/usr/share/evmapy
+    cp $(BR2_EXTERNAL_KNULLI_PATH)/package/emulators/dolphin-emu/*.keys \
+        $(TARGET_DIR)/usr/share/evmapy
+endef
+
+define DOLPHIN_EMU_PRE_CONFIGURE_HOOK
+    sed -i 's/set(DOLPHIN_VERSION_MAJOR .*)/set(DOLPHIN_VERSION_MAJOR "$(DOLPHIN_EMU_VERSION_MAJOR)")/' \
+        $(@D)/CMake/ScmRevGen.cmake
+    sed -i 's/set(DOLPHIN_VERSION_MINOR .*)/set(DOLPHIN_VERSION_MINOR "$(DOLPHIN_EMU_VERSION_MINOR)")/' \
+        $(@D)/CMake/ScmRevGen.cmake
+endef
+
+DOLPHIN_EMU_PRE_CONFIGURE_HOOKS = DOLPHIN_EMU_PRE_CONFIGURE_HOOK
+
+$(eval $(cmake-package))
