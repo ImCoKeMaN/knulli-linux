28230cb7ca7263daedaf68389f9e667a53923791
acmeplus <ctalon@gmail.com>
2026-06-19 21:36:36 +0000

    Updated knulli-notice package

--- external.mk
+++ external.mk
@@ -18,7 +18,6 @@ $(BR2_EXTERNAL_KNULLI_PATH)/package/emulators/*/*/*.mk \
 $(BR2_EXTERNAL_KNULLI_PATH)/package/firmwares/*/*.mk \
 $(BR2_EXTERNAL_KNULLI_PATH)/package/gpu/*/*.mk \
 $(BR2_EXTERNAL_KNULLI_PATH)/package/kernels/*/*.mk \
-$(BR2_EXTERNAL_KNULLI_PATH)/package/knulli-notice/*/*.mk \
 $(BR2_EXTERNAL_KNULLI_PATH)/package/libraries/*/*.mk \
 $(BR2_EXTERNAL_KNULLI_PATH)/package/modules/*/*.mk \
 $(BR2_EXTERNAL_KNULLI_PATH)/package/music/*/*.mk \
